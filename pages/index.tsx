b5b129b97bb9f3473bd25125a9b09013dd15fce5
Eric Wong <ericwong12@live.com>
2022-09-23 18:12:28 -0600

    Fix: Remove unused lines.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from "next";
-import 
+
 const Home: NextPage = () => {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
         <h1 className="text-2xl">What are we learning today?</h1>
-
       </div>
     </>
   );
