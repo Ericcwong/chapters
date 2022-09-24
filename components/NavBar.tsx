a61c5f5f3cba3b6eea22dc9bd8688888c1182cc6
Eric Wong <ericwong12@live.com>
2022-09-23 18:12:15 -0600

    Feat: New button boilerplate. Will add function to create a new cateogry.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,11 @@ import ThemeChanger from "./ThemeChanger";
 
 export default function NavBar() {
   return (
-    <div className="flex justify-end relative top-8 right-8">
+    <div className="flex justify-end relative gap-4 top-8 right-8">
+      {/* New Category */}
+      <button className=" bg-blue-600 rounded p-2">New Category</button>
+
+      {/* Light or Dark mode */}
       <ThemeChanger />
     </div>
   );
